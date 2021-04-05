9cca0e53e58d3e88bd4974cabae985e336327bfb
Jeremiasz Major <jrh.mjr@gmail.com>
2021-04-05 14:45:53 +0200

    implement Arrayable and Jsonable

--- src/CastableDataTransferObject.php
+++ src/CastableDataTransferObject.php
@@ -3,21 +3,23 @@
 namespace JessArcher\CastableDataTransferObject;
 
 use Illuminate\Contracts\Database\Eloquent\Castable;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
 use JessArcher\CastableDataTransferObject\Casts\DataTransferObject as DataTransferObjectCast;
 use Spatie\DataTransferObject\DataTransferObject;
 use function Safe\json_decode;
 use function Safe\json_encode;
 
-abstract class CastableDataTransferObject extends DataTransferObject implements Castable
+abstract class CastableDataTransferObject extends DataTransferObject implements Castable, Arrayable, Jsonable
 {
     public static function castUsing(array $arguments)
     {
         return new DataTransferObjectCast(static::class);
     }
 
-    public function toJson()
+    public function toJson($options = 0)
     {
-        return json_encode($this->toArray());
+        return json_encode($this->toArray(), $options);
     }
 
     public static function fromJson($json)
